78cb3c99fdf4cfdfd087637e4f5ef4bbe2f57885
suneelachinni <suneela.chinni@gmail.com>
2022-08-19 10:10:25 -0500

    First commit !

--- src/package2/SumAvg.java
+++ src/package2/SumAvg.java
@@ -1,5 +1,37 @@
 package package2;
 
+
+
+
 public class SumAvg {
+	 int reading;
+	 int math;
+	 int writing;
+	 int socialStudies;
+	 int science;
+	 int sum;
+	 float avg;
+	 
+	 void sum () {
+		 sum=reading+math+writing+socialStudies+science;
+	 }
+void avg() {
+	avg=sum/5;
+	
+}
+public static void mainH(String[] args) {
+	SumAvg student=new SumAvg();
+	student.reading=95;
+	student.math=99;
+	student.writing=75;
+	student.socialStudies=75;
+	student.science=90;
+student.sum();
+student.avg();
+System.out.println("student all subjects sum="+student.sum);
+System.out.println("student marks avg="+student.avg);
 
 }
+
+}
+
