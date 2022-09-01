a50d9a5dbd34a72117d6a5c1b3dbe7a91f7f91d3
suneelachinni <111553591+suneelachinni@users.noreply.github.com>
2022-08-31 21:35:14 -0500

    Add files via upload

---
+++ src/package2/module2.java
@@ -0,0 +1,20 @@
+package Testsuite1;
+
+import org.testng.annotations.Test;
+
+public class module2 {
+	@Test
+	public void test001() {
+		System.out.println("I am executing test001");
+	}
+	@Test
+	public void test002() {
+		System.out.println("I am executing test002");
+	}
+	@Test
+	public void test003() {
+		System.out.println("I am executing test003");
+	}
+	
+
+}
