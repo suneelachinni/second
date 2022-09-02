9d50967c584798c382200580197caac4015a4aa0
suneelachinni <111553591+suneelachinni@users.noreply.github.com>
2022-09-02 15:22:54 -0500

    Update cosmocode.java

--- src/package2/cosmocode.java
+++ src/package2/cosmocode.java
@@ -17,19 +17,21 @@ public class cosmocode {
 		driver.findElement(By.id("firstname")).sendKeys("suneela");
 		driver.findElement(By.className("lastname")).sendKeys("chinni");
 		//driver.findElement(By.linkText("Click here to reload this page")).click();
-		//driver.findElement(By.xpath(null))
+		
 		driver.findElement(By.name("language_java")).click();
 		driver.findElement(By.name("language_python")).click();
 		driver.findElement(By.name("language_c#")).click();
 		driver.findElement(By.name("language_c")).click();
 		driver.findElement(By.name("language_vbs")).click();
-		//driver.findElement(By.name(""))
+		driver.findElement(By.xpath("//input[@value='Female']")).click();
+		driver.findElement(By.xpath("//label[text()='First Name']"));
+		driver.findElement(By.xpath("/html/body/div[2]/div/div/main/article/div/div/input[11]")).click();
 		//driver.findElement(By.partialLinkText("submit_htmlform")).submit();
 		//driver.findElement(By.xpath("//inputf[@type='submit' and @value='Submit']")).click();
 	
 		//driver.findElement(By.linkText("random-text-xyz-i-wont-change-random-digit-123")).click();
 		//driver.findElement(By.linkText("Click Me to open New Window")).click();
-		driver.findElement(By.tagName("Under 20")).click();
+		
 		
 		
 		
